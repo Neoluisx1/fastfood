ff6ab09cfbcdb23033002c7d95094adceae9f9c3
Neoluisx1 <106831476+Neoluisx1@users.noreply.github.com>
2022-06-03 13:06:57 -0400

    Initial commit

---
+++ resources/views/layouts/theme/footer.blade.php
@@ -0,0 +1,5 @@
+<script src="{{asset('dist/js/app.js')}}"></script>
+<script src="{{asset('js/snackbar.min.js')}}"></script>
+<script src="{{asset('js/sweetalert2.min.js')}}"></script>
+<script src="{{asset('js/alpine.js')}}"></script>
+
---
+++ resources/views/layouts/theme/sidebar.b;ade.php
@@ -0,0 +1,59 @@
+<nav class="side-nav">
+                <a href="" class="intro-x flex items-center pl-5 pt-4">
+                    <img alt="logo" class="w-6" src="{{asset('dist/images/logo.svg')}}">
+                    <span class="hidden xl:block text-white text-lg ml-3"> Fast<span class="font-medium">Food</span> </span>
+                </a>
+                <div class="side-nav__devider my-6"></div>
+                <ul>
+                    <li>
+                        <a href="#" class="side-menu">
+                            <div class="side-menu__icon"> <i data-feather="layers"></i> </div>
+                            <div class="side-menu__title"> Categorias </div>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="#" class="side-menu">
+                            <div class="side-menu__icon"> <i data-feather="coffee"></i> </div>
+                            <div class="side-menu__title"> Productos </div>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="#" class="side-menu">
+                            <div class="side-menu__icon"> <i data-feather="plus-circle"></i> </div>
+                            <div class="side-menu__title"> Compras </div>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="#" class="side-menu">
+                            <div class="side-menu__icon"> <i data-feather="shopping-cart"></i> </div>
+                            <div class="side-menu__title"> Ventas </div>
+                        </a>
+                    </li>
+                    <div class="side-nav__devider my-6"></div>
+                    <li>
+                        <a href="#" class="side-menu">
+                            <div class="side-menu__icon"> <i data-feather="user"></i> </div>
+                            <div class="side-menu__title"> Clientes </div>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="#" class="side-menu">
+                            <div class="side-menu__icon"> <i data-feather="key"></i> </div>
+                            <div class="side-menu__title"> Usuarios </div>
+                        </a>
+                    </li>
+                    <div class="side-nav__devider my-6"></div>
+                    <li>
+                        <a href="#" class="side-menu">
+                            <div class="side-menu__icon"> <i data-feather="calendar"></i> </div>
+                            <div class="side-menu__title"> Reportes </div>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="#" class="side-menu">
+                            <div class="side-menu__icon"> <i data-feather="settings"></i> </div>
+                            <div class="side-menu__title"> Configuracion </div>
+                        </a>
+                    </li>
+                </ul>
+            </nav>
