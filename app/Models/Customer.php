93fcc30a4f44f3b0fd170e46b5a8b882a6642752
Neoluisx1 <106831476+Neoluisx1@users.noreply.github.com>
2022-06-06 22:08:04 -0400

    customer1

--- app/Models/Customer.php
+++ app/Models/Customer.php
@@ -4,8 +4,15 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Mockery\Matcher\Type;
 
 class Customer extends Model
 {
     use HasFactory;
+
+    protected $fillable=['name','phone','city','mail'];
+
+    public function orders(){
+        return $this->hasMany((Order::class));
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 use App\Http\Livewire\Categories;
+use App\Http\Livewire\Customers;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -15,6 +16,7 @@ use Illuminate\Support\Facades\Route;
 
 
 Route::get(uri:'categories', action:Categories::class)->name(name:'categories');
+Route::get(uri:'customers', action:customers::class)->name(name:'customers');
 
 Route::get('/', function () {
     return view('auth.login');
